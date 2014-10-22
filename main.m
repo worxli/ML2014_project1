28ab53bf4dcb77e94c3287d1832d5d72b820628d
Lukas Bischofberger <lukasbi@student.ethz.ch>
2014-10-22 22:15:20 +0200

    lukas ridge 0.31 - basline easy

--- main.m
+++ main.m
@@ -1,64 +1,89 @@
+clear;
+
 %% read data
 training = csvread('training.csv');
 %testdata = csvread('testing.csv');
 validation = csvread('validation.csv');
 
-data = training(:,1:end-1);
-y = training(:,end);
-
-no_buckets = 10;
-bucketsize = floor(size(training,1)/no_buckets); %% don't care about the last three
-
 %%  normalization
-range = [6 128 72 72 120 14 7 31000 768 24 960 960 7488 27]./2;
-mean = [5 96 44 44 100 9 4.5 16500 635 20 544 544 4256 22.5];
+%range = [6 128 72 72 120 14 7 31000 768 24 960 960 7488 27]./2;
+%mean = [5 96 44 44 100 9 4.5 16500 635 20 544 544 4256 22.5];
 
-averagedata = data - repmat(mean,333,1);
-x = bsxfun(@rdivide, averagedata, range);
-x = [x y]; 
+%averagedata = data - repmat(mean,333,1);
+%x = bsxfun(@rdivide, averagedata, range);
+%x = [x y]; 
+
+MEAN = mean(training);
+STD = std(training);
+averagedata = training-repmat(MEAN,size(training,1),1);
+normdata = bsxfun(@rdivide, averagedata, STD);
+
+X = normdata(:,1:end-1);
+y = normdata(:,end);
 
 
 %% ridge regression
 
-% cross validation for lambda
-err = [];
-err2 = [];
-betas = [];
-for i=1:no_buckets
+lambda = 0:0.05:10;%exp(-10:1:10);
+kfold = 10;
+
+candidates = [];
+for k=lambda
+
+    err = [];
+    betas = [];
+    candidate = [];
+    Xt = [ones(size(X,1),1) X];
+    ind = crossvalind('Kfold', size(X,1), kfold);
+   
+    for i = 1:kfold
+        Xts = Xt(ind == i, :);
+        Xtr = Xt(ind ~= i, :);
+        
+        beta = inv(Xtr'*Xtr+k*eye(size(Xtr,2)))*Xtr'*y(ind ~= i);
+        betas = [betas; beta'];
+        x = 1:size(Xts,1);
+        %plot(x, Xts*beta, x, y(ind == i));
+        %pause(0.5);
+        err = [err  norm(Xts*beta - y(ind == i))];
+    end
     
-    %validation set indices
-    valSetIndex = [(i-1)*bucketsize+1, i*bucketsize];
-    
-    %validation set
-    V = x(valSetIndex(1):valSetIndex(2),:);
-    Vy = V(:,end);
-    V = V(:,1:end-1);
-    
-    %training set w/o the validation set
-    X = [ones(size(x,1)-size(V,1),1) [x(1:valSetIndex(1)-1,:) ; x(valSetIndex(2)+1:end,:) ]];
-    y = X(:,end);
-    X = X(:,1:end-1);
-    
-    %beta = inv(X'*X+10^(i+3)*eye(size(X,2)))*X'*y;
-    beta = ridge(y,X,2^-i)
-    
-    
-    betas(i,:) = [beta];
-    err = [err min(abs(V*beta(1:end-1) - Vy))];
-    err2 = [err2 norm(V*beta(1:end-1) - Vy)];
-    
+    %plot(err);
+    %hold on;
+    [val,ind] = min(err);
+    candidate = [val betas(ind,:)];
+   
+    candidates = [candidates; candidate]; 
     
 end
 
-averagedata = validation - repmat(mean,333,1);
-normval = bsxfun(@rdivide, averagedata, range);
+[val, ind] = min(candidates(:,1));
+beta = candidates(ind,2:end);
 
-csvwrite('validationsetresult.csv', normval*betas(5,1:end-1)');
-
-
-plot(err);
-figure;
-plot(err2);
+ridgebeta = inv(X'*X+lambda(ind)*eye(size(X,2)))*X'*y;
 
 
-%% plotting
+
+plot(1:333, X*ridgebeta, 1:333, y);
+
+% test on validation set
+
+averagedata = validation-repmat(MEAN(1:end-1),size(validation,1),1);
+normdata = bsxfun(@rdivide, averagedata, STD(1:end-1));
+
+prediction = normdata*ridgbeta;
+unnormpred = bsxfun(@times, prediction, STD(end));
+preddata = unnormpred+repmat(MEAN(end),size(validation,1),1);
+
+csvwrite('validationsetresult.csv', preddata);
+
+
+
+
+
+
+
+
+
+
+
