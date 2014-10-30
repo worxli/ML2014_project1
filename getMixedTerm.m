2770e1fdd48c9c737bfd5903371b4c0e117b101d
Lukas Bischofberger <lukasbi@student.ethz.ch>
2014-10-30 08:18:22 +0100

    lukas tryouts

--- getMixedTerm.m
+++ getMixedTerm.m
@@ -1,10 +1,9 @@
 function [ MIX ] = getMixedTerm( X)
-MIX = [];
-% for i=1:14
-%    for j=i:14
-%        MIX = [MIX X(:,i).*X(:,j)];
-%    end
-% end
- MIX = [MIX X.^-3 X.^-1]; 
+    MIX = [];
+    for i=1:14
+        for j=i:14
+            MIX = [MIX X(:,i).*X(:,j)];
+        end
+    end
 end
 
