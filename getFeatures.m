abf34cab513da9c9c0624cdd31b975ce06b859e2
TheRiddance <andrin.jenal@gmail.com>
2014-10-31 18:30:06 +0100

    feature selection changes

--- getFeatures.m
+++ getFeatures.m
@@ -4,8 +4,8 @@ function [ F ] = getFeatures(X)
     %F = [ones(size(X,1),1) log(X) 1./X sqrt(X) X X.^2 X.^3 X.^5];
     %F = [ones(size(X,1),1) 1./X X X.^5];
     
-    %F = x2fx(X, 'quadratic');
+    F = x2fx(X, 'quadratic');
     
-    %F = [F MIX X.^3 X.^4 abs(log(X)) 1./X abs(sqrt(X)) X.^-2];
-    F = X;
+    F = [F X(:,1)./X(:,14) exp(X) abs(log(X)) abs(sqrt(X)) 1./X];
+
 end
--- getMixedTerm.m
+++ getMixedTerm.m
@@ -2,7 +2,7 @@ function [ MIX ] = getMixedTerm( X)
 MIX = [];
 for i=1:14
    for j=i:14
-       MIX = [MIX X(:,i).*X(:,j)];
+       MIX = [MIX X(:,i)./X(:,j)];
    end
 end
 
